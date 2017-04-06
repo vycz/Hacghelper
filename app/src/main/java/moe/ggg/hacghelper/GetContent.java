edd74221ac2c3a20550eb91ff98252db55e921ba
orange <orangeyao@outlook.com>
2017-04-06 23:49:14 +0800

    修正逻辑

--- app/src/main/java/moe/ggg/hacghelper/GetContent.java
+++ app/src/main/java/moe/ggg/hacghelper/GetContent.java
@@ -21,7 +21,7 @@ public class GetContent {
     public String get(){
         Document document = null;
         try {
-            document =  Jsoup.connect(Url).timeout(30000).get();
+            document =  Jsoup.connect(Url).timeout(30000).header("User-Agent","Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").get();
         } catch (IOException e) {
             e.printStackTrace();
         }
